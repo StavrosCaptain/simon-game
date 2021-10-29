7ddfbcd7fa2a206c760712daf169be520fc9bb19
StavrosCaptain <kali@CaptainStavr.localdomain>
2021-10-30 01:05:53 +0300

    fixed bugs

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-$("button").on("click", function () {
-    var green = new Audio("sounds/green.mp3");
-    green.play();
-})
+var userClickedPattern = [];
+
+var gamePattern = [];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+function random_number() {
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    var x = gamePattern.push(randomChosenColour);
+
+    $("#", randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio('sounds/' + randomChosenColour + '.mp3');
+    audio.play();
+
+}
+
+$(".btn").click(function () {
+
+    var userChosenColour = $(this).attr("id");
+
+    userClickedPattern.push(userChosenColour);
+
+    console.log(userClickedPattern);
+
+});
+
+
+
