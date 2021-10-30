25d16c399b1655161a43c576937d9c50a05ed7de
StavrosCaptain <kali@CaptainStavr.localdomain>
2021-10-30 22:23:20 +0300

    sound added

--- script.js
+++ script.js
@@ -4,17 +4,25 @@ var gamePattern = [];
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+var started = false;
+
+var level = 0;
+
 function random_number() {
+
+    level++;
+
+    $("#level-title").text("Level " + level)
 
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
-    var x = gamePattern.push(randomChosenColour);
+    gamePattern.push(randomChosenColour);
 
-    $("#", randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio('sounds/' + randomChosenColour + '.mp3');
-    audio.play();
+    playSound(userChosenColour);
 
+    checkAnswer(randomChosenColour);
 }
 
 $(".btn").click(function () {
@@ -23,9 +31,64 @@ $(".btn").click(function () {
 
     userClickedPattern.push(userChosenColour);
 
-    console.log(userClickedPattern);
+
+
+    playSound(userChosenColour);
+
+    animatePress(userChosenColour);
+    //2. Call checkAnswer() after a user has clicked and chosen their answer, passing in the index of the last answer in the user's sequence.
+    checkAnswer(userClickedPattern.length - 1);
 
 });
 
+$(document).keypress(function () {
+    if (!started) {
+        $("#level-title").text("Level " + level)
+    }
+    random_number();
+    started = true;
+});
 
 
+function playSound(name) {
+
+    var audio = new Audio('sounds/' + name + '.mp3');
+    audio.play();
+
+};
+
+function animatePress(currentColor) {
+
+    $("#" + currentColor).addClass("pressed");
+
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+
+};
+
+
+function checkAnswer(currentLevel) {
+
+    //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+        console.log("success");
+
+        //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+        if (userClickedPattern.length === gamePattern.length) {
+
+            //5. Call nextSequence() after a 1000 millisecond delay.
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+
+        }
+
+    } else {
+
+        console.log("wrong");
+
+    }
+
+}
