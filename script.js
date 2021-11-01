586c3721492889101b1396571c79631db42b6d60
StavrosCaptain <kali@CaptainStavr.localdomain>
2021-11-01 23:37:09 +0200

    Finished

--- script.js
+++ script.js
@@ -1,94 +1,77 @@
 var userClickedPattern = [];
-
 var gamePattern = [];
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var started = false;
-
 var level = 0;
 
+$(document).keypress(function () {
+    if (!started) {
+        $("#level-title").text("Level " + level)
+        random_number();
+        started = true;
+    }
+});
+
+$(".btn").click(function () {
+
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                random_number();
+            }, 1000);
+        }
+    } else {
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Press Any Key To Restart");
+        playSound("wrong");
+
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        startOver();
+    }
+}
+
 function random_number() {
-
+    userClickedPattern = [];
     level++;
-
-    $("#level-title").text("Level " + level)
-
+    $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-    playSound(userChosenColour);
-
-    checkAnswer(randomChosenColour);
+    playSound(randomChosenColour);
 }
 
-$(".btn").click(function () {
-
-    var userChosenColour = $(this).attr("id");
-
-    userClickedPattern.push(userChosenColour);
-
-
-
-    playSound(userChosenColour);
-
-    animatePress(userChosenColour);
-    //2. Call checkAnswer() after a user has clicked and chosen their answer, passing in the index of the last answer in the user's sequence.
-    checkAnswer(userClickedPattern.length - 1);
-
-});
-
-$(document).keypress(function () {
-    if (!started) {
-        $("#level-title").text("Level " + level)
-    }
-    random_number();
-    started = true;
-});
-
-
-function playSound(name) {
-
-    var audio = new Audio('sounds/' + name + '.mp3');
-    audio.play();
-
-};
-
 function animatePress(currentColor) {
-
     $("#" + currentColor).addClass("pressed");
-
     setTimeout(function () {
         $("#" + currentColor).removeClass("pressed");
     }, 100);
+}
 
-};
+function playSound(name) {
+    var audio = new Audio('sounds/' + name + '.mp3');
+    audio.play();
+}
 
-
-function checkAnswer(currentLevel) {
-
-    //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-
-        console.log("success");
-
-        //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
-        if (userClickedPattern.length === gamePattern.length) {
-
-            //5. Call nextSequence() after a 1000 millisecond delay.
-            setTimeout(function () {
-                nextSequence();
-            }, 1000);
-
-        }
-
-    } else {
-
-        console.log("wrong");
-
-    }
-
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
 }
